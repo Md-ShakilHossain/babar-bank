c4804cca799eaa09f781242d610b48ed7d9f1607
Md-ShakilHossain <shakilh550@gmail.com>
2023-08-15 09:12:53 +0600

    created login filed with validation

--- js/login.js
+++ js/login.js
@@ -1,1 +1,14 @@
-console.log("printed");
+document.getElementById('btn-login').addEventListener('click', function(){
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    emailField.value = '';
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    passwordField.value = '';
+    if(email === 'baaper@sontaan.com' && password === 'secret'){
+        console.log('Valid User');
+    }
+    else{
+        alert('Wrong Email or Password');
+    }
+})
